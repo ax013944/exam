edee50c9197ea5fef2e818e2ee5941b95fdab748
ax013944 <ax013944@gmail.com>
2018-01-16 14:20:08 +0800

    niner

---
+++ exam/src/edu/fju/exam/niner.java
@@ -0,0 +1,14 @@
+package edu.fju.exam;
+
+public class niner {
+
+	public static void main(String[] args) {
+		for(int i=1;i<6;i++){
+			for(int a=2;a<6;a++){
+			System.out.print(a+"x"+i+"="+a*i+"\t");
+			}
+		System.out.println();
+		}
+	}
+
+}
